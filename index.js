df759e311fbf57fffb510260e5977e3a5300efc6
SantiagoSosaReyes <santiago.sosa@uptc.edu.co>
2020-12-13 18:52:36 -0500

    Se reenvia una imagen a un servidor

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3001
+const FormData = require('form-data');
+const fs = require('fs');
+const axios = require('axios');
+const stream = fs.createReadStream('./archivos/imagenPrueba.png');
 
-app.use(express.static('public'));
+
+app.post('/', (req, res) => {
+  sendImage(res);
+})
+
+
+function sendImage(res) {
+  var data = new FormData();
+  data.append("myImage", stream);
+  axios.post('http://192.168.0.16:3000/subir', data, data.getHeaders())
+  .catch(function (error) {
+    console.log('Error ' + error.message)
+    res.send('Error')
+    return;
+  })
+  res.send('OK');
+}
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
