99dcc49f012eb00afbe0d66ca35161f9f1f2b426
SantiagoSosaReyes <santiago.sosa@uptc.edu.co>
2020-12-14 11:50:37 -0500

    subir imagen a otro servidor

--- index.js
+++ index.js
@@ -5,27 +5,74 @@ const FormData = require('form-data');
 const fs = require('fs');
 const axios = require('axios');
 const stream = fs.createReadStream('./archivos/imagenPrueba.png');
-let servers = ['http://192.168.0.15:3000/subir' , 'http://192.168.0.16:3000/subir']
+let servers = ['http://192.168.0.15:3000/' , 'http://192.168.0.16:3000/']
 let number = 0;
 
+var imagenPrueba = 'imagenPrueba.png';
+const path = require('path');
+const multer = require('multer');
+
+
+let storage = multer.diskStorage({
+  destination:(req, file, cb) =>{
+      cb(null, './archivos')
+  },
+  filename:(req, file, cb)=> {
+      cb(null, imagenPrueba);
+  }
+});
+
+const upload = multer({storage})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/**
+ * Subida de una imagen puede ser de otro servidor o desde un cliente
+ */
+app.post('/subir' , upload.single('file'), (req, res) => {
+  //Console.log(`Subiendo imagen..${req.hostname}/${req.file.path}`);
+  return res.send(req.file);
+})
+
+
 app.get('/', (req, res) => {
-  res.send(sendImage());
+  if(number != -1){
+    sendImage();
+    pedirDeVuelta();
+    res.send('Intentado enviar imagen y pedir de vuelta...');
+  }else {
+    res.send('Al menos un servidor esta fallando!!');
+  }
 })
 
 
 function sendImage() {
-  let respuesta = 'ERROR';
   console.log('Peticion a: ' + servers[number]);
   var data = new FormData();
   data.append("myImage", stream);
-  fromOtherServer = axios.post(servers[number], data, data.getHeaders())
+  fromOtherServer = axios.post(servers[number] + 'subir', data, data.getHeaders())
   .then(function (response) {
     console.log(response);
-    respuesta = 'OK';
+  })
+  .catch(function(error) {
+    console.log(error);
+    number = -1;
+  });
+}
+
+function pedirDeVuelta(){
+  axios.get(servers[number]+'devolver')
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.log(error);
+    number = -1;
   });
   sum();
-  return respuesta;
 }
+
 /**
  * Suma uno a la variable number
  */
