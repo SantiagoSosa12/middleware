32fcf79633281edb9fc13db853d8079ffdb78e43
SantiagoSosaReyes <santiago.sosa@uptc.edu.co>
2020-12-14 20:44:42 -0500

    se cambian los servidores

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
