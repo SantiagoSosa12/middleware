8c0051a205cc102ecf7a322e24459eda20500926
SantiagoSosaReyes <santiago.sosa@uptc.edu.co>
2021-02-20 15:23:44 -0500

    se cambia pug por html5

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ let servers = ['192.168.0.15' , '192.168.0.16']
 let number = 0;
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000}));
+/*app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 1000000}));*/
 
 
 app.use(express.json());
